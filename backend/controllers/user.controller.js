0fd40b05fd51b5277a9048727f23722cfb8471b0
Dhairya Shah <dhairyashah693@gmail.com>
2025-07-09 01:35:45 +0530

    user controller for cors

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -87,7 +87,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        secure: true,            
+        sameSite: "None",  
       })
       .json({
         message: `Welcome back ${user.fullName}`,
